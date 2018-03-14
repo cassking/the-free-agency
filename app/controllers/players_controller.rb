26a8c9c3aa141d328ead60e507137b023f4cfda0
Eric Ruan <wangmacanghao@yahoo.com>
2018-03-14 09:39:30 -0400

    minor change to the chreate_play branch

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,7 +1,3 @@
 class PlayersController < ApplicationController
-  def index
-    @players = Player.all
-  end
-  
-  def show; end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Player.delete_all
+Stat.delete_all
+
+james_harden = Player.create(
+  first_name: "James",
+  last_name: "Harden",
+  avatar_url: "https://specials-images.forbesimg.com/imageserve/5936925ea7ea434078d4c5eb/416x416.jpg?background=000000&cropX1=1335&cropX2=3965&cropY1=104&cropY2=2735",
+  age: "28",
+  height: "6'5",
+  weight: "220",
+  birth_city: "Los Angeles, CA",
+  birth_country: "USA",
+  position: "SG"
+)
+
+Stat.create(
+  player_id: james_harden.id,
+  ppg: "30",
+  apg: "9.4",
+  rpg: "5"
+)
+
+kevin_durant = Player.create(
+  first_name: "Kevin",
+  last_name: "Durant",
+  avatar_url: "http://tsnimages.tsn.ca/ImageProvider/PlayerHeadshot?seoId=kevin-durant&width=620&height=620",
+  age: "29",
+  height: "6'9",
+  weight: "240",
+  birth_city: "Washington, D.C.",
+  birth_country: "USA",
+  position: "SF"
+)
+
+Stat.create(
+  player_id: kevin_durant.id,
+  ppg: "30",
+  apg: "9.4",
+  rpg: "5"
+)
