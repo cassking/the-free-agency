4e63edf1bcf29bd4b6a401a673bb4d25888fdca8
Eric Ruan <wangmacanghao@yahoo.com>
2018-03-15 10:42:13 -0400

    code climate changes

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,14 +1,11 @@
 class Api::V1::PlayersController < ApplicationController
   def index
-
     render json: Player.all
   end
 
   def show
-
     @player= Player.find(params[:id])
     @comments = Comment.where(player_id: params[:id])
-    render json: {:player => @player, :comments => @comments }
+    render json: { :player => @player, :comments => @comments }
   end
-
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,14 +9,12 @@ require 'shoulda-matchers'
 require File.join(File.dirname(__FILE__), 'support/valid_attribute')
 require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
-
-
-  Shoulda::Matchers.configure do |config|
-    config.integrate do |with|
-      with.test_framework :rspec
-      with.library :rails
-    end
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
   end
+end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
