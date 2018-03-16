368d4bfb5c9e6409f86bb900297e48e78c961afb
Jacob Merrell <mostlytoes@Jacob-Scarf-Merrell.local>
2018-03-16 16:30:22 -0400

    codeclimate

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -6,8 +6,8 @@ class Api::V1::PlayersController < ApplicationController
   def show
     @player= Player.find(params[:id])
     @comments = Comment.where(player_id: params[:id])
-    @c = @comments.map {|comment| [comment, comment.user.username] }
+    @c = @comments.map { |comment| [comment, comment.user.username] }
     @stat = Stat.find_by(player_id: params[:id])
-    render json: { player: @player, comments: @c, stat: @stat}
+    render json: { player: @player, comments: @c, stat: @stat }
   end
 end
