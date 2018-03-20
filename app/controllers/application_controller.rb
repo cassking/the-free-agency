bace56ef802e973ec2392482e8107d3f18569382
Jacob Merrell <jacobscarfmerrell@gmail.com>
2018-03-20 09:55:46 -0400

    deleted redundant protect_from_forgery line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   helper :all
-  protect_from_forgery with: :exception
 
   # protect_from_forgery prepend: true
   protect_from_forgery unless: -> { request.format.json? }
