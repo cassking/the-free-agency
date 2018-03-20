0d16c99b8dd4a42ac30865bef979da953c32be64
Eric Ruan <wangmacanghao@yahoo.com>
2018-03-20 13:17:22 -0400

    deleted a redundant file

--- app/models/stats.rb
+++
@@ -1,5 +0,0 @@
-# define player stat, which belongs to the player. one to one relationship
-class Stat < ApplicationRecord
-  belongs_to :player
-  validates_presence_of :ppg, :apg, :rpg, :player_id
-end
