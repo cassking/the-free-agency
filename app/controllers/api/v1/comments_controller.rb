2784c0762b06c6e5a0fad70859b3636307b44c2b
Jacob Merrell <jacobscarfmerrell@gmail.com>
2018-03-22 19:33:08 -0400

    init commit

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -2,24 +2,20 @@ class Api::V1::CommentsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @signed_in = user_signed_in?
     @comments = Comment.where(player_id: params[:player_id])
-    @comments_sorted = @comments.sort_by do |comment|
-      sum = 0
-      comment.votes.each do |vote|
-        sum += vote.up_or_down
-      end
-      sum
-    end
-    @userVotes =[]
+    @userVotes = []
     @comments.each do | comment |
       if comment.votes
         comment.votes.each do | vote |
-          if (vote.user_id === current_user.id)
-            @userVotes << vote
-          end
+          @userVotes << vote if (vote.user_id === current_user.id)
         end
       end
+    end
+
+    @comments_sorted = @comments.sort_by do |comment|
+      sum = 0
+      comment.votes.each { |vote| sum += vote.up_or_down }
+      sum
     end
     @comments_sorted.reverse!
     @comments_with_username = @comments_sorted.map do |comment|
@@ -28,6 +24,7 @@ class Api::V1::CommentsController < ApplicationController
         votes: comment.votes
       }
     end
+    @signed_in = user_signed_in?
     render json: {
       comments: @comments_with_username,
       signed_in: @signed_in,
